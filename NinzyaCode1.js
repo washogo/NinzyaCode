374606797bfb08d3186237522173a7526db630b3
washogo <aiz4338150@gmail.com>
2021-11-05 20:28:50 +0900

    [Add]4/18未学習

--- NinzyaCode1.js
+++ NinzyaCode1.js
@@ -1,16 +1,9 @@
 'use strict';
 
 {
-  // let text1 = "リンゴ・バナナ";
-  // let text2 = "レモン";
-  // let fruits = (text1 + text2).replace("バナナ","");
-  
-  // alert(fruits);
-
-  let text1 = "リンゴ・バナナ・ミカン";
-  let text2 = "レモン";
-  let fruits = (text1 + text2).replace("バナナ","梨");
-  let more_fruits = (fruits).replace('レモン','・レモン')
-  
-  alert(more_fruits);
+  $(function(){
+    $('li').on('click',function(){
+      alert($(this).attr('class'));
+    });
+  });
 }
