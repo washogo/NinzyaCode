2d7a2c54d9cf4df98265693e5e1e687f000736a2
washogo <aiz4338150@gmail.com>
2021-11-05 21:08:45 +0900

    [Add]7/18

--- NinzyaCode1.js
+++ NinzyaCode1.js
@@ -1,12 +1,7 @@
 'use strict';
 
 {
-  // let fruits = ["リンゴ", "メロン", "バナナ"];
-  // alert(fruits[1]);
-
-  const fruits = ["リンゴ", "メロン", "バナナ",];
-  // alert(fruits[1]);
-  alert(fruits[0]);
-  alert(fruits[2]);
-
+  $(function(){
+    alert($('li').length);
+  })
 }
